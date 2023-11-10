55b15851620690aedba7ca683db8b63920df5181
felipefariagoncalves <felipe.angra@hotmail.com>
2023-11-10 13:23:29 +0000

    Exercício Atualizado

---
+++ desafio.js
@@ -0,0 +1,10 @@
+let nome = "Black Hero"
+let quantXp = 1000
+
+switch (quantXp){
+    case 1000:
+    quantXp = "Ferro"
+    break
+}
+console.log("O herói de nome", nome, "está no nível de",quantXp)
+
